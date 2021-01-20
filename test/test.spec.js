5042981bfb7fb0178fa4443a509fa22f9c4e3032
phant <phantrunghieu0510@gmail.com>
2021-01-19 22:53:42 -0500

    updated testing

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,16 +1,15 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const chaiString = require('chai-string');
-const { server } = require("../app");
+const server = require("../app.js");
 const expect = chai.expect;
-const {readFileSync} = require('fs');
+const fs = require('fs');
 require('dotenv').config()
 
 chai.use(chaiHttp).use(chaiString);
 
-const validTokenMiami = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1MmZjYjk3ZGY1YjZiNGY2ZDFhODg1ZjFlNjNkYzRhOWNkMjMwYzUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIxMDYyNTU3NTA4MDg2LTQ0ajQwdnU3ZzBkZzM0cGkzMmFlNmtxM2Fyam02bzFqLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMTA2MjU1NzUwODA4Ni00NGo0MHZ1N2cwZGczNHBpMzJhZTZrcTNhcmptNm8xai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExNjI3MTAzMjcyNzUyNTEwNDAwNCIsImhkIjoibWlhbWlvaC5lZHUiLCJlbWFpbCI6InBoYW5oQG1pYW1pb2guZWR1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJ0TVptZ2RCLW5LX2xDZW1sTlc5S1BnIiwibm9uY2UiOiJGVHNaUkpvb3hkWkJEUnFqYXJJZUp0MmxRSkVYNzMiLCJuYW1lIjoiSGlldSBQaGFuIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS8tdGotOWVZMDlUeUEvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQU1adXVjbG5jbVliZjJtV1VyMUxqM3M0blFwOTg4anB3QS9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoiSGlldSIsImZhbWlseV9uYW1lIjoiUGhhbiIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNjEwMjM2NTYxLCJleHAiOjE2MTAyNDAxNjEsImp0aSI6IjZjNTM3ZjcxYmIwNzdlZmFhNmNjMjFiZTZjNzUxMzFiN2YzYmQyNDMifQ.rxDzf-FhOCbyKzXYVsJFNBQvINBJR5RwZ5mtmBA-40Iq2KPff6jT7p01C8VJjXsrNVwARge_5CYNDwIpSRh6fCohkosYIOPYZ1YXxOtZioyhqPjcex5qRF6TeaQBAGtQ5A-ONrfRwmnRSVVo-zz2Ewc6crbEhT6ADDXIg8GN_a9Asb9CvuraLFwJJ3lRJRVLAYBj5dFjTHzBfnygzAp7eU0tF00RVRkckSOrDmLXM252l6mTwZrH4gTpTDc2OwtZmQhZ2Jtoj6ooglWsMgjK7ngM2LQGm4SFENpxXUZ9i_gFsa8IHT1N2YD7ztw0SeS3ez2qVhUtuKqmr1fpRBqTtw"
+const validToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc4M2VjMDMxYzU5ZTExZjI1N2QwZWMxNTcxNGVmNjA3Y2U2YTJhNmYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIxMDYyNTU3NTA4MDg2LTQ0ajQwdnU3ZzBkZzM0cGkzMmFlNmtxM2Fyam02bzFqLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMTA2MjU1NzUwODA4Ni00NGo0MHZ1N2cwZGczNHBpMzJhZTZrcTNhcmptNm8xai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExNjI3MTAzMjcyNzUyNTEwNDAwNCIsImhkIjoibWlhbWlvaC5lZHUiLCJlbWFpbCI6InBoYW5oQG1pYW1pb2guZWR1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJ5RnBpcmlQQlM4ZWJKVVByN0twR1J3Iiwibm9uY2UiOiI2VXg4VFNMMVpnMTRtdWFWTWRMRXpYbm4xbDM4cmYiLCJuYW1lIjoiSGlldSBQaGFuIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS8tdGotOWVZMDlUeUEvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQU1adXVjbG5jbVliZjJtV1VyMUxqM3M0blFwOTg4anB3QS9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoiSGlldSIsImZhbWlseV9uYW1lIjoiUGhhbiIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNjExMTE0NzMzLCJleHAiOjE2MTExMTgzMzMsImp0aSI6ImY2MWE4NWQzNDNhMzgxZDQyOWE5Y2FiNjEwMGM0NDUwZjE1NzliZTIifQ.BrzJUpHzxA9gFrNQiZwgnaEw1vGWzGZM_E3ok3qBBJkrOhGBzzF8QtC_upbF12FUAs0b11S_6PWTsDTjsm4Wi7DkbmO0iS4eBBQiLHbdCJfoPgNos4nGCRgAoaQbyX4xJypWmVomluuzUm8OxcyGLWAHPAx3IkO5ueLguMaxNfZXbVTjMt_qC115xLNgMJV9SHFK50uNESN0r2wxSgU3uZAFCBoy-NJtHpXH7QQr5wb8SCuPq8j6ZG975IkaKhKZdTVXTTuQoFXxbhLOescVPZpehnldeXyzRkg5BvLFaGkpOyz3D8qO1F9LM7lxTRs3si0SuJ9tFKzYdLHrQUiawQ"
 const invalidToken = "abcd"
-const validTokenNonMiami = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1MmZjYjk3ZGY1YjZiNGY2ZDFhODg1ZjFlNjNkYzRhOWNkMjMwYzUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIxMDYyNTU3NTA4MDg2LTQ0ajQwdnU3ZzBkZzM0cGkzMmFlNmtxM2Fyam02bzFqLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMTA2MjU1NzUwODA4Ni00NGo0MHZ1N2cwZGczNHBpMzJhZTZrcTNhcmptNm8xai5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExNTg0NzM4NjI1MjAwOTI5NTg3NyIsImVtYWlsIjoicGhhbnRydW5naGlldTA1MTBAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJqRXUyVlZJTnN2OW9qQ09kX3ptLVpRIiwibm9uY2UiOiJBU1JLZ0RFQ2xYMmRFSGV0dzA4cVlkelhJRHRkdmwiLCJuYW1lIjoiSGnhur91IFBoYW4iLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2dHUXd4REtuU005OUZTMXZvd3pNanNMbV9fNFQxd09qcGtvTWc9czk2LWMiLCJnaXZlbl9uYW1lIjoiSGnhur91IiwiZmFtaWx5X25hbWUiOiJQaGFuIiwibG9jYWxlIjoidmkiLCJpYXQiOjE2MTAyMzY3MDUsImV4cCI6MTYxMDI0MDMwNSwianRpIjoiMmFjY2NiMmZkZmRlMWRkNmI5ODE1MTZjNzcyYThkOTg0NGE3NjBkMCJ9.kkCkm6y8Ga9rtNu7UYlPco9apOwSMqjFJoogOfS6nfLx-RBGW4ygm9iGS3ZJaIuZ1ko5yKMc0e16yKXPqPdbtrbWq3xTgkGEaEnOeOjgYb2ILs3bFUhWTNU60Dh8lOnt3rLp5xghIggu9j60m_NSUOKR3l74SOD52e1kgu40GgnK5arzUdGGAZ1Hb4GXNZg-3Nw32TE2pLNj8FbqtF0QaDR6bmxcrFDSuLqf0IvsQMPLd_j1Slf93bbpMsW8M3UvMPNyu642CK3cTrlp4Q0tMcXAPSHsVZ86pekZ_Jt8xVNXbIW7T7JU-kY0aY7S040c2tlgQUIVEEIhOj3Bkpryog"
 
 describe("App", () => {
     after(() => {
@@ -22,7 +21,7 @@ describe("App", () => {
       chai
         .request(server)
         .get("/api/home")
-        .set('X-Authentication', validTokenMiami)
+        .set('X-Authentication', validToken)
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.text).equal("Hello World");
@@ -43,27 +42,15 @@ describe("App", () => {
     });
 
     // test /api/signin route
-    it("should get signin successfully with Miami account token", done => {
+    it("should get signin successfully with valid token", done => {
       chai
         .request(server)
         .get("/api/signin")
-        .set('X-Authentication', validTokenMiami)
-        .query({token: validTokenMiami})
+        .set('X-Authentication', validToken)
+        .query({token: validToken})
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.text).equal("Success");
-          done();
-        });
-    });
-
-    it("should not get signin successfully with non-Miami account token", done => {
-      chai
-        .request(server)
-        .get("/api/signin")
-        .set('X-Authentication', validTokenNonMiami)
-        .query({token: validTokenNonMiami})
-        .end((err, res) => {
-          expect(res).to.have.status(403);
           done();
         });
     });
@@ -73,7 +60,8 @@ describe("App", () => {
       chai
         .request(server)
         .post("/api/image")
-        .attach('image', readFileSync('./website.jpg'), 'website.jpg')
+        .set('X-Authentication', validToken)
+        .attach('image', fs.readFileSync('./test-image.jpg'), 'test-image.jpg')
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.text).to.have.string(
@@ -87,6 +75,7 @@ describe("App", () => {
       chai
         .request(server)
         .post("/api/image")
+        .set('X-Authentication', validToken)
         .end((err, res) => {
           expect(res).to.have.status(403);
           expect(res.text).equal("Error");
